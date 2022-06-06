fe8f904b80987dfb8e4186a6de57084d9ec9cccd
Jimi <contact@jimi.sexy>
2022-06-06 14:03:39 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-import fs from 'fs';
-import fetch from 'node-fetch';
+const cities = JSON.parse(Deno.readTextFileSync('./cities.json'));
 
-const citys = JSON.parse(fs.readFileSync('./citys.json', 'utf8'));
-
-for (const city in citys) {
-    // info@distance24.org
-    fetch(`https://www.distance24.org/route.json?stops=${city}|Berlin`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
+function beforeGame() {
+    const playerAmnt = prompt('Wie viele Spieler*innen gibt es?');
+    if (!playerAmnt || !parseInt(playerAmnt) || parseInt(playerAmnt) > 5 || parseInt(playerAmnt) < 2) {
+        console.clear();
+        console.log('Bitte eine Zahl von 2 - 5 eingeben');
+        const bg = beforeGame();
+        return bg;
+    } else {
+        const startingPoints = [];
+        for (let i = 0; i < cities.length; i++) {
+            if (cities[i].y === 0) {
+                startingPoints.push(i);
+            }
         }
-    }).then(res => console.log(res));
-        /*res.json()).then(json => {
-        console.log(city, json.distance);
-    }).catch(err => {
-        console.log(err);
-    });*/
+        return { startingPoint: startingPoints[Math.floor(Math.random() * startingPoints.length)], players: parseInt(playerAmnt) };
+    }
 }
+
+function move(turn, y) {
+    let moves = [];
+    for (let i = 0; i < cities.length; i++) {
+        if (cities[i].y === y + 1) {
+            moves.push(i);
+        }
+    }
+    moves = moves.sort(function(){return .5 - Math.random()});
+    moves = moves.slice(0,3);
+    moves.forEach((element, index) => {
+        console.log(`${index}: ${cities[element].name}, ${cities[element].state}`)
+    });
+}
+console.clear();
+const bGameReturn = beforeGame();
+const players = bGameReturn?.players;
+const startingPoint = bGameReturn?.startingPoint;
+
+console.log(`Ihr startet in: ${cities[startingPoint].name}, ${cities[startingPoint].state}`);
+move(1, 0);
